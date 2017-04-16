347f35679594d1c4427b5b1188a9fe29c5161e5c
Purna Chamala <purna.chamala@dell.com>
2017-04-16 03:40:18 -0500

    Update githubRelease.groovy

--- vars/githubRelease.groovy
+++ vars/githubRelease.groovy
@@ -6,7 +6,7 @@ def call() {
     def utils = new com.dell.cpsd.SCM.Utils()
     def repoName = utils.getRepoName()
     
-    if (env.BRANCH_NAME ==~ /master|release\/.*/) {
+    if (env.BRANCH_NAME ==~ /release\/.*/) {
         dir('/opt'){
             sh "rm -f linux-amd64-github-release.tar.bz2"
             sh "wget https://github.com/aktau/github-release/releases/download/v0.7.2/linux-amd64-github-release.tar.bz2"
@@ -23,7 +23,7 @@ def call() {
                     --repo ${repoName} \
                     --tag v0.0.1-${BRANCH_NAME}-${BUILD_ID} \
                     --name '${repoName} release' \
-                    --file ${WORKSPACE}/target/${repoName}-0.0.1-SNAPSHOT.jar"
+                    --file ${WORKSPACE}/target/${repoName}-1.0-SNAPSHOT.jar"
         }
     }
     else {
