2cd15d579d0b98bc49584c3bd170fd69edb9eff5
Purna Chamala <purna.chamala@dell.com>
2017-04-15 23:07:48 -0500

    Update githubRelease.groovy

--- vars/githubRelease.groovy
+++ vars/githubRelease.groovy
@@ -1,24 +1,26 @@
 #!/usr/bin/groovy
 
 def call() {
-    dir('/opt'){
-        sh "rm -f linux-amd64-github-release.tar.bz2"
-        sh "wget https://github.com/aktau/github-release/releases/download/v0.7.2/linux-amd64-github-release.tar.bz2"
-        sh "tar -xvjf linux-amd64-github-release.tar.bz2"
-        sh "yes | cp bin/linux/amd64/github-release /usr/bin/"
-        sh '''
-            github-release release \
-                --user dellemc-symphony \
-                --repo  rcm-evaluation-service-api \
-                --tag v0.0.1-${BRANCH_NAME}-${BUILD_ID} \
-                --name "rcm-evaluation-service-api release" \
-                --description "rcm-evaluation-service-api release"
-            github-release upload \
-                --user dellemc-symphony \
-                --repo rcm-evaluation-service-api \
-                --tag v0.0.1-${BRANCH_NAME}-${BUILD_ID} \
-                --name "rcm-evaluation-service-api release" \
-                --file ${WORKSPACE}/target/rcm-evaluation-service-api-1.0-SNAPSHOT.jar
-        '''
+    if (env.BRANCH_NAME ==~ /release\/.*/) {
+        dir('/opt'){
+            sh "rm -f linux-amd64-github-release.tar.bz2"
+            sh "wget https://github.com/aktau/github-release/releases/download/v0.7.2/linux-amd64-github-release.tar.bz2"
+            sh "tar -xvjf linux-amd64-github-release.tar.bz2"
+            sh "yes | cp bin/linux/amd64/github-release /usr/bin/"
+            sh '''
+                github-release release \
+                    --user dellemc-symphony \
+                    --repo  rcm-evaluation-service-api \
+                    --tag v0.0.1-${BRANCH_NAME}-${BUILD_ID} \
+                    --name "rcm-evaluation-service-api release" \
+                    --description "rcm-evaluation-service-api release"
+                github-release upload \
+                    --user dellemc-symphony \
+                    --repo rcm-evaluation-service-api \
+                    --tag v0.0.1-${BRANCH_NAME}-${BUILD_ID} \
+                    --name "rcm-evaluation-service-api release" \
+                    --file ${WORKSPACE}/target/rcm-evaluation-service-api-1.0-SNAPSHOT.jar
+            '''
+        }
     }
 }
