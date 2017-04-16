97817d7960476f183ffdeedfcd96124a1904486c
Purna Chamala <purna.chamala@dell.com>
2017-04-16 03:42:21 -0500

    Update doThirdPartyAudit.groovy

--- vars/doThirdPartyAudit.groovy
+++ vars/doThirdPartyAudit.groovy
@@ -1,7 +1,6 @@
 #!/usr/bin/groovy
 
-def call() {
-    
+def call() {    
     sh '''
         mvn org.apache.maven.plugins:maven-dependency-plugin:2.10:analyze-report license:add-third-party org.apache.maven.plugins:maven-dependency-plugin:2.10:tree \
         -DoutputType=dot \
