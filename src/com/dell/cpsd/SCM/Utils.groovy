13fddaa424ae92997424dd7779e932c397900625
Purna Chamala <purna.chamala@dell.com>
2017-04-20 18:28:15 -0500

    Update Utils.groovy

--- src/com/dell/cpsd/SCM/Utils.groovy
+++ src/com/dell/cpsd/SCM/Utils.groovy
@@ -4,10 +4,10 @@ package com.dell.cpsd.SCM
 
 def getRepoName() {
   
-  if (env.JOB_NAME ==~ /dellemc.*/) {
+  if (env.JOB_NAME ==~ /^dellemc.*/) {
     return env.JOB_NAME.replaceAll("dellemc-symphony/", "").replaceAll("/${BRANCH_NAME}", "")
   }
-  else if (env.JOB_NAME ==~ /vce.*/) {
+  else if (env.JOB_NAME ==~ /^vce.*/) {
     return env.JOB_NAME.replaceAll("vce-symphony/", "").replaceAll("/${BRANCH_NAME}", "")
   }
 }
