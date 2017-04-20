65d47efaa02bf18d6e2c6a9738c4fd1a5946fc29
Purna Chamala <purna.chamala@dell.com>
2017-04-20 15:52:31 -0500

    Create doNexbScanning2.groovy

---
+++ vars/doNexbScanning2.groovy
@@ -0,0 +1,23 @@
+#!/usr/bin/groovy
+import com.dell.cpsd.SCM.Utils
+
+def call() {    
+    
+    def utils = new com.dell.cpsd.SCM.Utils()
+    def repoName = utils.getRepoName2()    
+    
+    dir('/opt') {
+            checkout([$class: 'GitSCM', 
+            branches: [[name: '*/master']], 
+            doGenerateSubmoduleConfigurations: false, 
+            extensions: [[$class: 'RelativeTargetDirectory', relativeTargetDir: 'nexB']], 
+            submoduleCfg: [], 
+            userRemoteConfigs: [[url: 'https://github.com/nexB/scancode-toolkit.git']]])
+    }	    
+    sh "mkdir -p /opt/nexB/nexb-output/"
+    sh "sh /opt/nexB/scancode --help"
+    sh "sh /opt/nexB/scancode --format html ${WORKSPACE} /opt/nexB/nexb-output/${repoName}.html"
+    sh "sh /opt/nexB/scancode --format html-app ${WORKSPACE} /opt/nexB/nexb-output/${repoName}-grap.html"	       
+    sh "mv /opt/nexB/nexb-output/ ${WORKSPACE}/"
+    archiveArtifacts '**/nexb-output/**'    
+}
