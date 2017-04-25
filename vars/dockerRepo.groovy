1ca332ab3b4a6be1951343afa970132a4b464aa0
Purna Chamala <purna.chamala@dell.com>
2017-04-25 15:00:38 -0500

    Update dockerRepo.groovy

--- vars/dockerRepo.groovy
+++ vars/dockerRepo.groovy
@@ -1,4 +1,10 @@
+#!/usr/bin/groovy
 
 public class Contact implements Serializable {
+    private string DOCKER_REGISTRY = "docker-dev-local.art.local"
+    
+    def getDockerRegistryName() {
+        return DOCKER_REGISTRY
+    }
 
 }
