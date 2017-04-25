721b0daddbefec13213c90fc88c5d9fe986203ec
Purna Chamala <purna.chamala@dell.com>
2017-04-25 14:41:26 -0500

    Update githubRelease.groovy

--- vars/githubRelease.groovy
+++ vars/githubRelease.groovy
@@ -3,30 +3,32 @@ import com.dell.cpsd.SCM.Utils
 
 def call() {
     
-    def utils = new com.dell.cpsd.SCM.Utils()
-    def repoName = utils.getRepoName()
-    
-    if (env.BRANCH_NAME ==~ /release\/.*/) {
-        dir('/opt'){
-            sh "rm -f linux-amd64-github-release.tar.bz2"
-            sh "wget https://github.com/aktau/github-release/releases/download/v0.7.2/linux-amd64-github-release.tar.bz2"
-            sh "tar -xvjf linux-amd64-github-release.tar.bz2"
-            sh "yes | cp bin/linux/amd64/github-release /usr/bin/"
-            sh "github-release release \
-                    --user dellemc-symphony \
-                    --repo ${repoName} \
-                    --tag v0.0.1-${BRANCH_NAME}-${BUILD_ID} \
-                    --name '${repoName} release' \
-                    --description '${repoName} release'"
-            sh "github-release upload \
-                    --user dellemc-symphony \
-                    --repo ${repoName} \
-                    --tag v0.0.1-${BRANCH_NAME}-${BUILD_ID} \
-                    --name '${repoName} release' \
-                    --file ${WORKSPACE}/target/${repoName}-1.0-SNAPSHOT.jar"
-        }
-    }
-    else {
-        println("Github Release skipped! Branch name pattern did not match 'release/*'.")
-    }
+    sh "echo Github Release stage skipped!"
+
+//    def utils = new com.dell.cpsd.SCM.Utils()
+//    def repoName = utils.getRepoName()
+//    
+//    if (env.BRANCH_NAME ==~ /release\/.*/) {
+//        dir('/opt'){
+//            sh "rm -f linux-amd64-github-release.tar.bz2"
+//            sh "wget https://github.com/aktau/github-release/releases/download/v0.7.2/linux-amd64-github-release.tar.bz2"
+//            sh "tar -xvjf linux-amd64-github-release.tar.bz2"
+//            sh "yes | cp bin/linux/amd64/github-release /usr/bin/"
+//            sh "github-release release \
+//                    --user dellemc-symphony \
+//                    --repo ${repoName} \
+//                    --tag v0.0.1-${BRANCH_NAME}-${BUILD_ID} \
+//                    --name '${repoName} release' \
+//                    --description '${repoName} release'"
+//            sh "github-release upload \
+//                    --user dellemc-symphony \
+//                    --repo ${repoName} \
+//                    --tag v0.0.1-${BRANCH_NAME}-${BUILD_ID} \
+//                    --name '${repoName} release' \
+//                    --file ${WORKSPACE}/target/${repoName}-1.0-SNAPSHOT.jar"
+//        }
+//    }
+//    else {
+//        println("Github Release skipped! Branch name pattern did not match 'release/*'.")
+//    } 
 }
