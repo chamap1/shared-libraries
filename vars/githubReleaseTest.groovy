e4139f8592a3a26eaab8351297121353dc0e2063
Purna Chamala <purna.chamala@dell.com>
2017-07-10 12:44:56 -0500

    Update githubReleaseTest.groovy

--- vars/githubReleaseTest.groovy
+++ vars/githubReleaseTest.groovy
@@ -13,11 +13,11 @@ def call() {
     if (env.BRANCH_NAME ==~ /release\/.*/) {
         sh "echo https://api.github.com/repos/${orgName}/${repoName}/releases"
         sh """
-            curl -i -H 'Authorization: token 222fd1a2dcbfa35fa92e4c9c29314b977f29990f' \
+            curl -i -H 'Authorization: token ${GITHUB_TOKEN}' \
             -d '{ \
             "tag_name": "v1.0.9002", \
             "target_commitish": "master", \
-            "name": "v1.0.9002 testing", \
+            "name": "${repoName} testing", \
             "body": "Description of the rel testing", \
             "draft": false, \
             "prerelease": false \
