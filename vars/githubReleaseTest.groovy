eeec44390b49fb8b23c3ae75748b5a8323daa17e
Purna Chamala <purna.chamala@dell.com>
2017-07-10 15:07:53 -0500

    Update githubReleaseTest.groovy

--- vars/githubReleaseTest.groovy
+++ vars/githubReleaseTest.groovy
@@ -1,25 +1,22 @@
 #!/usr/bin/groovy
 import com.dell.cpsd.SCM.Utils
 
-def repoName = "travis-ci-tutorial-java"
-def orgName = "chamap1"
-
-def call() {
+def call(String tag_name = '', String name = 'test_name', String body = '') {
 
     def utils = new com.dell.cpsd.SCM.Utils()
 //    def repoName = utils.getRepoName()
 //    def orgName = utils.getOrgName()
-
-    
-    
+        def repoName = "travis-ci-tutorial-java"
+        def orgName = "chamap1"
+        
     if (env.BRANCH_NAME ==~ /release\/.*/) {
         sh "echo https://api.github.com/repos/${orgName}/${repoName}/releases"
         sh """
             curl -i -H 'Authorization: token ${GITHUB_TOKEN}' \
             -d '{ \
-            "tag_name": "v1.0.9002", \
+            "tag_name": "v1.0.0.${BUILD_ID}", \
             "target_commitish": "master", \
-            "name": "${repoName} testing", \
+            "name": "${name}", \
             "body": "Description of the rel testing", \
             "draft": false, \
             "prerelease": false \
