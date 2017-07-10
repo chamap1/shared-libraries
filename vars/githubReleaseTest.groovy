5d077e3516f58089198a30f14f6ffac86a019284
Purna Chamala <purna.chamala@dell.com>
2017-07-10 15:17:06 -0500

    Update githubReleaseTest.groovy

--- vars/githubReleaseTest.groovy
+++ vars/githubReleaseTest.groovy
@@ -1,23 +1,35 @@
 #!/usr/bin/groovy
 import com.dell.cpsd.SCM.Utils
 
-def call(String tag_name = '', String name = 'test_name', String body = '') {
+def call(String tag_name = '', String name = '', String body = '') {
 
     def utils = new com.dell.cpsd.SCM.Utils()
 //    def repoName = utils.getRepoName()
 //    def orgName = utils.getOrgName()
-        def repoName = "travis-ci-tutorial-java"
-        def orgName = "chamap1"
-        
+    def repoName = "travis-ci-tutorial-java"
+    def orgName = "chamap1"
+    
+    if (tag_name == ''){
+        tag_name = "v1.0.0.${BUILD_ID}"
+    }
+    
+    if (name == ''){
+        name = "${repoName} Release"
+    }
+    
+    if (body == ''){
+        body = "${repoName} Release"
+    }
+       
     if (env.BRANCH_NAME ==~ /release\/.*/) {
         sh "echo https://api.github.com/repos/${orgName}/${repoName}/releases"
         sh """
             curl -i -H 'Authorization: token ${GITHUB_TOKEN}' \
             -d '{ \
-            "tag_name": "v1.0.0.${BUILD_ID}", \
+            "tag_name": "${tag_name}", \
             "target_commitish": "master", \
             "name": "${name}", \
-            "body": "Description of the rel testing", \
+            "body": "${body}", \
             "draft": false, \
             "prerelease": false \
             }' \
