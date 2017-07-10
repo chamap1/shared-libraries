a49ab59dcfc0d7dad0e442d35ba286e116474146
Purna Chamala <purna.chamala@dell.com>
2017-07-10 12:25:40 -0500

    Update githubReleaseTest.groovy

--- vars/githubReleaseTest.groovy
+++ vars/githubReleaseTest.groovy
@@ -12,7 +12,8 @@ def call() {
     
     if (env.BRANCH_NAME ==~ /release\/.*/) {
         sh "echo https://api.github.com/repos/${orgName}/${repoName}/releases"
-        sh "curl -i -H 'Authorization: token 222fd1a2dcbfa35fa92e4c9c29314b977f29990f' \
+        sh """
+            curl -i -H 'Authorization: token 222fd1a2dcbfa35fa92e4c9c29314b977f29990f' \
             -d '{ \
             "tag_name": "v1.0.9002", \
             "target_commitish": "master", \
@@ -21,7 +22,8 @@ def call() {
             "draft": false, \
             "prerelease": false \
             }' \
-            https://api.github.com/repos/${orgName}/${repoName}/releases"
+            https://api.github.com/repos/${orgName}/${repoName}/releases
+            """
     }
     else {
         println("Github Release skipped! Branch name pattern did not match 'release/*'.")
